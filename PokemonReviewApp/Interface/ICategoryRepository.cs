feb943ce06f44d3e3365b475fa9b4d0439678fad
Hnat Liashenko <hnat.liashenko@bluepes.com>
2023-07-20 11:15:06 +0300

    create category and country controllers with http get endpoints

---
+++ PokemonReviewApp/Interface/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interface
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+
+    }
+}
